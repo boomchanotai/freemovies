6ee1a84f49787207539f3d22df1b009f2ef2a00d
matcha <meen.sankaew@gmail.com>
2021-01-02 09:10:26 +0700

    add movie search api

---
+++ api/search_video.js
@@ -0,0 +1,28 @@
+const axios = require("axios");
+const jsdom = require("jsdom");
+const { async } = require("window-or-global");
+module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  const _res = await axios.get(
+    "https://movie285.com/?s=" + encodeURI(req.body.search)
+  );
+  const dom = new jsdom.JSDOM(_res.data);
+  let out = [];
+  dom.window.document.querySelectorAll(".col-3.list-box-item").forEach((a) => {
+    out.push({
+      title: a
+        .getElementsByClassName("list-link")[0]
+        .getElementsByClassName("list-box")[0]
+        .getElementsByClassName("list-title")[0].innerHTML,
+      href: a
+        .getElementsByClassName("list-link")[0]
+        .href.replace("https://movie285.com/", ""),
+      pic: a
+        .getElementsByClassName("list-link")[0]
+        .getElementsByClassName("list-box")[0]
+        .getElementsByClassName("list-thumbnail")[0]
+        .getElementsByClassName("attachment-full")[0].src,
+    });
+  });
+  res.send(out);
+};
